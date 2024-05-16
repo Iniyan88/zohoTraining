0c5a3772747f0458c4ad98f2f6f6b75141e5121c
Iniyan88 <biniyan2004@gmail.com>
2024-05-16 16:35:29 +0530

    first commit

--- character.c
+++ character.c
@@ -1,16 +1,42 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+
 int main(){
-    char a[10]="a6b2c1";
+    char a[10]="a11b12c13";
     int n=strlen(a);
-    int range=0;
-    printf("%d\n",n);
-    for(int i=0;i<n;i+=2)
-    {
-         range=  (a[i+1])-48;
-        for(int j=0;j<range;j++)
+    printf("%d",n);
+    char c;
+    char s[100];
+    int flag=0;
+    int value=0;
+    char b=a[0];
+    char temp;
+    for(int i=0;i<n;i++)
+    {        
+        if(a[i]>='a' && a[i]<='z')
         {
-            printf("%c",a[i]);
+            c=a[i];
+        } else{
+            temp=c;
+            s[flag++]=a[i];  
         }
+        if(a[i]>='a' && a[i]<='z')
+        {  
+            value=atoi(s);
+            strcpy(s," ");
+            flag=0;
+             for(int k=0;k<value;k++)
+             {                
+                printf("%c",temp);
+             }
+             temp=a[i];
+        }           
     }
+    for(int i=0;i<value;i++)
+    {
+        printf("%c",temp);
+    }
+   
+
 }
